18632832be4e8e3932d10b769216d5051908eb28
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-01 21:53:58 -0400

    notes.get added, along with the initialization of fs

---
+++ routes/notes.js
@@ -0,0 +1,7 @@
+const notes = require('express').Router();
+const fs = require('fs')
+
+notes.get('/', (req, res) => {
+    fs.readFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
+});
+
