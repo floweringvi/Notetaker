09e03dc2a8f293b6f247133e9659ccd811185bd7
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-06 09:49:29 -0400

    here

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,12 @@ const notes = require('express').Router();
 const fs = require('fs')
 
 notes.get('/', (req, res) => {
-    fs.readFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
+    fs.readFile('./db/notes.json')
+    .then((data) => res.json(JSON.parse(data)))
 });
 
-notes.post()
+//Should this be built out, writing into the file? As in exactly?
+notes.post('/', (req, res) => {
+    fs.writeFile('./db/notes.json')
+    .then((data)=>res.json(JSON.parse(data)))
+});
